d9953291c34fe68eda1a04f814f2c0c504e2995e
acruz2332 <akbar.fajar2301@mail.ugm.ac.id>
2023-03-30 15:40:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.localetextstarter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText harga;
+    TextView hasil;
+    Button btn;
+
+    int result = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHelp();
+            }
+        });
+
+        Date myDate = new Date();
+        long expirationDate = myDate.getTime() + TimeUnit.DAYS.toMillis(5);
+        myDate.setTime(expirationDate);
+
+        String formatDate = DateFormat.getDateInstance().format(myDate);
+        TextView expiredTextView = findViewById(R.id.date);
+        expiredTextView.setText(formatDate);
+
+        harga = findViewById(R.id.etHarga);
+        hasil = findViewById(R.id.tvHasil);
+        btn = findViewById(R.id.btnHitung);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String nilai = harga.getText().toString();
+                    result = (int) Integer.parseInt(nilai);
+                }catch (NumberFormatException e){
+                    harga.setError("Masukan angka!");
+                }
+
+
+                NumberFormat formats = NumberFormat.getCurrencyInstance();
+                formats.setMaximumFractionDigits(0);
+
+                formats.setCurrency(Currency.getInstance(Locale.getDefault()));
+
+                String final_val = formats.format(result*100);
+                hasil.setText(final_val);
+            }
+        });
+    }
+
+    /**
+     * Shows the Help screen.
+     */
+    private void showHelp() {
+        // Create the intent.
+        Intent helpIntent = new Intent(this, HelpActivity.class);
+        // Start the HelpActivity.
+        startActivity(helpIntent);
+    }
+
+    /**
+     * Creates the options menu and returns true.
+     *
+     * @param menu       Options menu
+     * @return boolean   True after creating options menu.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles options menu item clicks.
+     *
+     * @param item      Menu item
+     * @return boolean  True if menu item is selected.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle options menu item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.action_help) {
+            Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.action_language){
+            Intent languageIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(languageIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
